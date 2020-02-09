d6b4e6663f959a3f5fb25c26b604ef30b009e028
webscrubbing <60850344+webscrubbing@users.noreply.github.com>
2020-02-10 00:59:35 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const waitFor = sec => new Promise(r => setTimeout(() => r(sec), sec * 1000));
+
+document.getElementById('go').addEventListener('click', async (e) => {
+    const url = document.getElementById('url').value;
+    const urlObj = new URL(url);
+    const withoutQueries = urlObj.origin + urlObj.pathname;
+
+    const tryCount = 3;
+    let count = 1;
+    while(true) {
+        if (tryCount === count) {
+            location.href = withoutQueries;
+            break;
+        }
+
+        await fetch(withoutQueries).then(r => r.text);
+        await waitFor(1);
+        console.log(count);
+        count++;
+    }
+});
